5a9d550d898197e26d8123ea72c3295b40739ea5
Watson <watson@watosonnoMacBook-puro.local>
2018-11-04 12:21:28 +0900

    Add container sample

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,2 +1,12 @@
 class MenuController < ApplicationController
+  def index
+  end
+
+  # container：固定枠
+  def container
+  end
+
+  # container-fluid：固定枠
+  def container_fluid
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "menu#index"
+
+  # menu controller routing
+  get "menu/container"
+  post "menu/container"
+
+  get "menu/container_fluid"
+  post "menu/container_fluid"
 end
